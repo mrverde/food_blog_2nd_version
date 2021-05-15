f16c28b5f348d62861d5ad6bb498ad380574e63a
Pepe Mateo <jdmatvaz@gmail.com>
2021-05-15 17:28:22 +0200

    Changed quality values imagemin.config.js

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 10 },
-    "mozjpeg": { "progressive": true, "quality": 10 },
-    "pngquant": { "quality": [0.25, 0.5] },
+    "mozjpeg": { "progressive": true, "quality": 30 },
+    "pngquant": { "quality": [0.30] },
     "svgo": {
         "plugins": [
             { "removeViewBox": false },
             { "cleanupIDs": true },
         ]
     },
-    "webp": { "quality": 10 }
+    "webp": { "quality": 30 }
 }
